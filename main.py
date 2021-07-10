0a0b0bd57948013c0633cf18e3f99500d5b99101
Nicolenewsoft <barrosnicole094@gmail.com>
2021-07-09 22:11:32 -0300

    building the cells with the data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import xlwt
+import shutil
 
 class GetNUmbers():
     def __init__(self, initial_number, final_numbers):
         self.initial_number = initial_number
-        self.final_numbers = final_numbers
+        self.final_numbers = final_numbers + 1
 
     def compose_list(self):
         list = []
@@ -13,12 +14,24 @@ class GetNUmbers():
         #print(list)
 
         wb = xlwt.Workbook()
+        style0 = xlwt.easyxf('pattern: pattern solid, fore_colour aqua;')
+
         ws = wb.add_sheet('Teste')
 
-        ws.write(0, 0, str(list))
+        titles = ['Números', 'Nome', 'Assunto', 'Símbolo', 'Observações', 'Data']
+
+        for i in range(len(titles)):
+            ws.write(0, i, titles[i], style0)
+
+        for i in list:
+            ws.write(i, 0, i)
 
         wb.save('spreadsheet.xls')
 
 
-x = GetNUmbers(1, 5)
+x = GetNUmbers(int(input("Digite o primeiro número: ")), int(input("Digite o último número: ")))
+print("Preparando sua planilha...")
 x.compose_list()
+shutil.move('spreadsheet.xls','/home/usuario/Área de trabalho')
+print("Sua planilha está pronta! ")
+
